b49f0e631e0fe4d37e5aa255ac735c54b0144820
アウン <hlumaungphyo@gmail.com>
2024-09-24 12:27:04 +0900

    Initial Commit

---
+++ Gohan/GohanApp.swift
@@ -0,0 +1,17 @@
+//
+//  GohanApp.swift
+//  Gohan
+//
+//  Created by Hlwan Aung Phyo on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct GohanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
